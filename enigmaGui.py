281475ba025e8922a4024c12d0d397c6b8d519ed
vishnudk <vishnudk179@gmail.com>
2021-10-30 13:59:36 +0530

    roter rotation

--- enigmaGui.py
+++ enigmaGui.py
@@ -1,7 +1,12 @@
 import tkinter as tk
+from tkinter.constants import COMMAND
 import mapingCharacters
+from typing import Literal
+import string
+
+alp = list(string.ascii_uppercase)
 r = tk.Tk()
-r.geometry("750x550")
+r.geometry("850x550")
 r.title('Enigma')
 inputText = tk.Text(r,height=10,width=50)
 outputText = tk.Text(r,height=10,width=50)
@@ -18,28 +23,35 @@ s3 = tk.Scale( r,
 def encrypt():
     rtPos = [s1.get()-1, s2.get()-1, s3.get()-1]
     outputText.delete(1.0,tk.END)
-    outputText.insert(tk.END, mapingCharacters.encryptFunction(inputText.get(1.0, "end-1c"),rtPos))
-def decrypt():
-    rtPos = [s1.get()-1, s2.get()-1, s3.get()-1]
-    outputText.delete(1.0,tk.END)
-    outputText.insert(tk.END,mapingCharacters.decryptFunction(inputText.get(1.0, "end-1c"),rtPos))
+    textData = inputText.get(1.0, "end-1c")
+
+    data = mapingCharacters.encryptFunction(textData,rtPos)
+    print("===================================")
+    s1.set(data[0][0]+1)
+    s2.set(data[0][1]+1)
+    s3.set(data[0][2]+1)
+    for i in range(3):
+        print(data[0][i])
+    outputText.insert(tk.END, data[1])
+
 def mainWindow():
     rotersLabel = tk.Label(r,text="Rotor Adjustment")
     plainTextLabel = tk.Label(r,text = "Plain Text")
     cipherTextLabel = tk.Label(r,text ="Cipher Text")
     encryptBtn = tk.Button(r,text = "ENCRYPT/DECRYPT",width=20,height=2,command= encrypt)
     # decryptBtn = tk.Button(r,text = "DECRYPT",width=10,height=5,command= decrypt)
-    rotersLabel.grid(row =0, column = 2)
-    inputText.grid(row = 1,column = 0)
-    outputText.grid(row = 3,column = 0)
-    encryptBtn.grid(row = 4,column = 0)
+    rotersLabel.grid(row =0, column = 3)
+    inputText.grid(row = 1,column = 1)
+    outputText.grid(row = 3,column = 1)
+    encryptBtn.grid(row = 4,column = 1)
     # decryptBtn.grid(row = 4,column = 1)
-    s1.grid(row = 1,column =1)
-    s2.grid(row = 1,column =2)
-    s3.grid(row = 1,column =3)
-    plainTextLabel.grid(row =0 , column =0)
-    cipherTextLabel.grid(row =2 , column =0)
+    s1.grid(row = 1,column =2)
+    s2.grid(row = 1,column =3)
+    s3.grid(row = 1,column =4)
+    plainTextLabel.grid(row =0 , column =1)
+    cipherTextLabel.grid(row =2 , column =1)
     r.mainloop()
 
 if __name__ == '__main__':
     mainWindow()
+    
--- mapingCharacters.py
+++ mapingCharacters.py
@@ -36,23 +36,30 @@ def init(rtPos):
         rotorsList.append(tmp)
     # print(rotorsList)
     alp.reverse()
-def rotate():
+def rotate(rtPos):
     tmp = rotorsList[0].pop(0)
     rotorsList[0].append(tmp)
+    rtPos[0] += 1
+    if rtPos[0] == 26:
+        rtPos[0] = 0
     if rotorsList[0][0] == "Z":
         tmp = rotorsList[1].pop(0)
         rotorsList[1].append(tmp)
+        rtPos[1] += 1
+        if rtPos[1] == 26:
+            rtPos[1] = 0
         if rotorsList[1][0] == "Z":
             tmp = rotorsList[2].pop(0)
             rotorsList[2].append(tmp)
-    
+            rtPos[2] += 1
+            if rtPos[2] == 26:
+                rtPos[2] = 0
+    return rtPos
     
 def encrypt(dir ,char , rotNo):
     if dir == "f":
-        
         return rotorsList[rotNo][alp.index(char)]
     else:
-        
         return alp[rotorsList[rotNo].index(char)]
 
 
@@ -68,9 +75,9 @@ def encryptFunction(c, rtPos):
         for i in range(2):
             tmp = encrypt('b',tmp,1-i)
 
-        rotate()
+        rtPos = rotate(rtPos)
         ciplist.append(tmp)
-    return ("".join(ciplist))
+    return [rtPos,"".join(ciplist)]
     
 def decryptFunction(cipheText,rtPos):
     cipheText = preProcess(cipheText)
@@ -83,6 +90,6 @@ def decryptFunction(cipheText,rtPos):
             tmp = encrypt('f',tmp,i)
         for i in range(3):
             tmp = encrypt('b',tmp,2-i)
-        rotate()
+        rtPos = rotate(rtPos)
         plainText.append(tmp)
-    return ("".join(plainText))
+    return [rtPos,"".join(plainText)]
